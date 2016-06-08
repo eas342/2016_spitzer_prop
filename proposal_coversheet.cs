c9f7f479ed22b7e092564e4769fa66a5c1bc7c51
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2016-06-08 01:59:34 -0700

    I added a proposal coversheet with an abstract, title, Co-Is etc.

---
+++ proposal_coversheet.cs
@@ -0,0 +1,89 @@
+Proposal.version= 6.0
+Proposal.proposalType= go
+Proposal.isTargetOfOpportunity= false
+Proposal.isThesis= false
+Proposal.requestedHours= 32.6
+Proposal.period= 365
+#------------------------- Title -------------------------------------
+Proposal.title= \
+     Characterizing the Dust from Disintegrating Planets
+
+#------------------------- Science Categories ------------------------
+Proposal.sciCategory.galactic= extrasolar planets
+#------------------------- Science Justification File ----------------
+Proposal.ScienceJustification.File= /Users/everettschlawin/Documents/proposals/2016_spitzer_cycle13/proposal_doc/cycle13_RegSnap_v1.pdf
+#------------------------- PI Information ----------------------------
+Proposal.pi.firstName= Everett
+Proposal.pi.lastName= Schlawin
+Proposal.pi.street.1= Department of Astronomy/Steward Observatory 
+Proposal.pi.street.2= 933 North Cherry Avenue
+Proposal.pi.city= Tucson
+Proposal.pi.state= Arizona
+Proposal.pi.country= United States
+Proposal.pi.zip= 85721
+Proposal.pi.phone= 520-621-2727
+Proposal.pi.fax= 520-621-1532
+Proposal.pi.email= eas342@email.arizona.edu
+Proposal.pi.dept= Steward Observatory
+Proposal.pi.institute= University of Arizona
+#------------------------- Contact Information -----------------------
+Proposal.contact.firstName= Everett
+Proposal.contact.lastName= Schlawin
+Proposal.contact.street.1= Department of Astronomy/Steward Observatory 
+Proposal.contact.street.2= 933 North Cherry Avenue, Rm. N268
+Proposal.contact.city= Tucson
+Proposal.contact.state= Arizona
+Proposal.contact.country= United States
+Proposal.contact.zip= 85721
+Proposal.contact.phone= 520-621-2727
+Proposal.contact.fax= 520-621-1532
+Proposal.contact.email= eas342@email.arizona.edu
+Proposal.contact.dept= Steward Observatory
+Proposal.contact.institute= University of Arizona
+#------------------------- Co-Investigator Information ---------------
+Proposal.coi.count= 6
+Proposal.coi.0.firstName= Bryce
+Proposal.coi.0.lastName= Croll
+Proposal.coi.0.institute= Boston University
+Proposal.coi.0.email= croll@bu.edu
+Proposal.coi.1.firstName= Johanna
+Proposal.coi.1.lastName= Teske
+Proposal.coi.1.institute= Carnegie Institution of Washington
+Proposal.coi.1.email= jteske@carnegiescience.edu
+Proposal.coi.2.firstName= Saul
+Proposal.coi.2.lastName= Rappaport
+Proposal.coi.2.institute= MIT
+Proposal.coi.2.email= sar@mit.edu
+Proposal.coi.3.firstName= Terry
+Proposal.coi.3.lastName= Herter
+Proposal.coi.3.institute= Cornell
+Proposal.coi.3.email= tlh10@cornell.edu
+Proposal.coi.4.firstName= John
+Proposal.coi.4.lastName= DeVore
+Proposal.coi.4.institute= Visidyne
+Proposal.coi.4.email= devore@visidyne.com
+Proposal.coi.5.firstName= Ming
+Proposal.coi.5.lastName= Zhao
+Proposal.coi.5.institute= Penn State University
+Proposal.coi.5.email= astro.mingzhao@gmail.com
+#------------------------- Abstract ----------------------------------
+Proposal.abstract= \
+     The Kepler Observatory uncovered a new class of disintegrating planets.\
+     \ These planets offer a way to study interior layers of exoplanets as th\
+     ey are stripped down from larger radii. We will characterize the dusty \
+     effluents coming off two sibling disintegrating planets KIC 12557548b a\
+     nd K2-22b. The dust particles escaping these two planets are still larg\
+     ely a mystery in terms of composition and particle size. Spitzer photom\
+     etry will probe the larger dust particle sizes when combined with optic\
+     al ground-based data. We will measure the spectral slope between 0.6 um\
+     \ and 3.6 um to constrain the size of particles escaping the two planets\
+     . The geometry of the tails of debris escaping these two planets has a \
+     different shape -- we will compare the particles from the two planets t\
+     o assess the causes of the different geometry. Three transits are neede\
+     d to average over the intrinsic variability of these systems and ensure\
+     \ a deep enough transit occurs for detection. Any new wavelength informa\
+     tion on these planet candidates will dramatically increase our knowledg\
+     e as there remain many mysteries about their composition and what mecha\
+     nism causes the planet to lose mass.
+
+#------------------------- Extra Information --------------------------
